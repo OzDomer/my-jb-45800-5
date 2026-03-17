18899ee9f6852a2b8b1326fca2f0eb1810497a10
Oz Domer <ozdomer7@gmail.com>
2026-03-17 15:59:06 +0200

    updated component folder

---
+++ 2026-03-17/classwork/vite-project/src/components/app/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quo consequuntur, aut odit vero ex quidem fugiat iste suscipit expedita.</p>
+  )
+}
+
+export default App
